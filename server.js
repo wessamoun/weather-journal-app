a99bfeb83922ac485a8f5b2f3d7cad3fce66ca78
wessamoun <weso_1998@yahoo.com>
2021-10-23 18:35:44 +0200

    Project Environment Setup

--- server.js
+++ server.js
@@ -2,18 +2,24 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require("body-parser");
+const cors = require("cors");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+let port = 8000;
+const server = app.listen (port, listening)
+function listening () {
+  console.log("Server is running in port 8000")
+}
